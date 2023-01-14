4010cb838ae9d72e26e3852a7de43db1290f98bd
dilberhusainkhan <dilberjmi@gmail.com>
2023-01-14 23:59:35 +0530

    Route update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import { Product } from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
 
+const Layout = ()=>{
+  return(
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/products/:id",
-    element: <Products/>,
-  },
-  {
-    path: "/product/:id",
-    element: <Product/>,
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>,
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>,
+      }
+    ]
   },
 ]);
 
