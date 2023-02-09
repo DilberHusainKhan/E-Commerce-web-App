96883330416b8c3c56015497558e4addee25ef41
dilberhusainkhan <dilberjmi@gmail.com>
2023-02-09 23:04:39 +0530

    Update

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -4,29 +4,53 @@ import './Product.scss';
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 
 export const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
-  const [quantity, setQuantity] = useState(0);
-  const data = [
-    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  ];
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
+  const [quantity, setQuantity] = useState(1);
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+
 
   return (
     <div className='product'>
-      <div className="left">
-        <div className="images">
-            <img src={data[0]} alt="" onClick={e=>setSelectedImg(0)}/>
-            <img src={data[1]} alt="" onClick={e=>setSelectedImg(1)}/>
-        </div>
-        <div className="mainImg">
-          <img src={data[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <h1>Title</h1>
+{ loading?("Loading.."):(
+<>
+
+<div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
+        </div>      
+          <div className="right">
+          <h1>Title</h1>
         <span className='price'>$199</span>
         <p>Dolor et takimata gubergren erat elitr rebum. Erat diam diam lorem et justo ea justo clita sanctus, kasd ut kasd.</p>
         <div className="quantity">
@@ -58,7 +82,7 @@ export const Product = () => {
           <hr />
           <span>FAQ</span>
         </div>
-      </div>
+      </div></>)}
     </div>
   )
 }
