5e7ce2bcd3df7f654df87ac93901c343973f746e
dilberhusainkhan <dilberjmi@gmail.com>
2023-02-19 13:58:06 +0530

    Update

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,52 +1,70 @@
-import React from 'react'
-import './Cart.scss'
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import {useSelector} from 'react-redux';
-import { removeItem, resetCart } from '../../redux/cartReducer';
+import React from "react";
+import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useSelector } from "react-redux";
+import { removeItem, resetCart } from "../../redux/cartReducer";
 import { useDispatch } from "react-redux";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
+
 const Cart = () => {
-    // const data = [
-    //     {
-    //         id:1,
-    //         title:"Long Sleeve Graphic T-Shirt",
-    //         isNew:true,
-    //         img:"https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    //         img2:"https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    //         oldPrice:20,
-    //         desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting, remaining essentially unchanged.",
-    //         price:15
-    //     },
-    // ]
-    const products = useSelector(state=>state.cart.products);
-    const dispatch = useDispatch();
-    const totalPrice =()=>{
-        let total =0;
-        products.forEach(element => (total += element.quantity*element.price));
-        return total.toFixed(2);
-    } 
+  const products = useSelector((state) => state.cart.products);
+  const dispatch = useDispatch();
 
+  const totalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
+    });
+    return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_51McYewSCV8OO4RZHoTb3hCMh6cr0ducSt4uOmgqf94NIHXGRoXK4MHrufqRtkUQlGW3abulL3JfmowjkQmQ0T98S00h54xssqF"
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className='cart'>
-        <h1>Product is in your cart</h1>
-         {products.map(item=>(
-            <div className="item" key={item.id}>
-                <img src={process.env.REACT_APP_UPLOAD_URL +item.img} alt="" />
-                <div className="details" >
-                    <h1>{item.title}</h1>
-                    <p>{item.desc?.substring(0,50)}</p>
-                    <div className="price">{item.quantity}x &#x20B9;{item.price}</div>
-                </div>
-                <DeleteOutlinedIcon className='delete' onClick={()=>dispatch(removeItem(item.id))}/>
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">
+              {item.quantity} x ₹{item.price}
             </div>
-         ))}
-        <div className="total">
-            <span>SUBTOTAL</span>
-            <span>&#x20B9;{totalPrice()}</span>
+          </div>
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
         </div>
-        <button>PROCEED TO CHECKOUT</button>
-        <span className="reset" onClick={()=> dispatch(resetCart())}>Reset Cart</span>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>₹{totalPrice()}</span>
+      </div>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
